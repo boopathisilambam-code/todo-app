613e64ab5774189c52d4ec21d177fa9da5bc25db
boopathisilambam-code <boopathisilambam@gmail.com>
2025-12-31 14:11:41 +0530

    Update Signup.jsx

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -18,7 +18,7 @@ export default function Signup() {
     setError('');
     setLoading(true);
     try {
-      await axios.post('http://localhost:5000/api/signup', { email, password });
+      await axios.post('https://todo-znes.onrender.com/api/signup', { email, password });
       setSuccess(true);
       setTimeout(() => navigate('/login'), 2000);
     } catch (err) {
