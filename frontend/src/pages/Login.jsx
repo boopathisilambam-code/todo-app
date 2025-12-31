5b503145f31c5ad169dc03a8d60d837b2c6aeae5
boopathisilambam-code <boopathisilambam@gmail.com>
2025-12-31 15:45:51 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -19,7 +19,7 @@ export default function Login() {
     setError('');
     setLoading(true);
     try {
-      const res = await axios.post('${import.meta.env.VITE_API_URL}/api/login', { email, password });
+      const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/login`, { email, password });
       login(res.data.token);
       navigate('/todo');
     } catch (err) {
